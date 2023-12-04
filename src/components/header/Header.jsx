87c75f8e46b9d3bd6d99fd8476895d63d6f1b7e5
vmeghna <shrimeghna97@gmail.com>
2023-12-04 18:03:31 +0530

    marvelView-MovieApp is completed

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -14,7 +14,7 @@ import { ToastContainer, toast } from "react-toastify";
 import "./style.scss";
 
 import ContentWrapper from "../contentWrapper/ContentWrapper";
-import logo from "../../assets/movix-logo.svg";
+import logo from "../../assets/movix-logo.png";
 
 const Header = () => {
   const { userEmail, logoutUser } = useUser();
